6995dc81eff9666d3b1b7a1f75420cd818f24a4e
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-21 13:05:27 +0530

    Refractor: Created object globally

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -10,18 +10,17 @@ import org.junit.jupiter.api.Test;
 
 public class CalculateDiscountTest {
 
+	CalculateDiscount calculateDiscount = new CalculateDiscount();
 	public static final String SOFTWARE_DEVELOPMENT_BOOK_I = "Clean Code (Robert Martin, 2008)";
 	public static final String SOFTWARE_DEVELOPMENT_BOOK_II = "The Clean Coder (Robert Martin, 2011)";
 
 	@Test
 	public void initializeToBuyBook() {
-		CalculateDiscount calculateDiscount = new CalculateDiscount();
 		assertNotNull(calculateDiscount);
 	}
 
 	@Test
 	public void buyingOneBook() {
-		CalculateDiscount calculateDiscount = new CalculateDiscount();
 		List<String> bookList = new ArrayList<>();
 		bookList.add(SOFTWARE_DEVELOPMENT_BOOK_I);
 		double discount = 0.00;
@@ -30,7 +29,6 @@ public class CalculateDiscountTest {
 
 	@Test
 	public void buyingTwoCopiesOfDifferentBook() {
-		CalculateDiscount calculateDiscount = new CalculateDiscount();
 		List<String> bookList = new ArrayList<>();
 		double discount = 5;
 		bookList.add(SOFTWARE_DEVELOPMENT_BOOK_I);
