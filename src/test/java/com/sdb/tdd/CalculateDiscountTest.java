51f1a78ba6113db21c26f62cb83f030e7d06ef84
LENOVO <LENOVO@DESKTOP-2MREFRI>
2023-06-21 12:56:07 +0530

    Add:Buying 2 different books

--- src/test/java/com/sdb/tdd/CalculateDiscountTest.java
+++ src/test/java/com/sdb/tdd/CalculateDiscountTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 public class CalculateDiscountTest {
 
 	public static final String SOFTWARE_DEVELOPMENT_BOOK_I = "Clean Code (Robert Martin, 2008)";
+	public static final String SOFTWARE_DEVELOPMENT_BOOK_II = "The Clean Coder (Robert Martin, 2011)";
 
 	@Test
 	public void initializeToBuyBook() {
@@ -25,6 +26,16 @@ public class CalculateDiscountTest {
 		bookList.add(SOFTWARE_DEVELOPMENT_BOOK_I);
 		double discount = 0.00;
 		assertEquals(50.0, calculateDiscount.getTotalPrice(bookList, discount));
+	}
+
+	@Test
+	public void buyingTwoCopiesOfDifferentBook() {
+		CalculateDiscount calculateDiscount = new CalculateDiscount();
+		List<String> bookList = new ArrayList<>();
+		double discount = 5;
+		bookList.add(SOFTWARE_DEVELOPMENT_BOOK_I);
+		bookList.add(SOFTWARE_DEVELOPMENT_BOOK_II);
+		assertEquals(95.0, calculateDiscount.getTotalPrice(bookList, discount));
 	}
 
 }
